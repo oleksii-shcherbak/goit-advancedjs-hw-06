b71497688a37573ca2b41a1cd4d9ebbe89b2efb0
Oleksii Shcherbak <oleksii_shcherbak@icloud.com>
2025-11-12 23:00:11 +0200

    Completed Task 2

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -1,9 +1,9 @@
 /*
-  У вас є функція merge, яка поєднує два об'єкти. 
+  У вас є функція merge, яка поєднує два об'єкти.
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
 
